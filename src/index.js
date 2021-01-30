5537fe8ee08f85b8a723eb5618d7caff256b499c
Hakan Yücel <hknyucell@gmail.com>
2021-01-30 15:23:03 +0300

    is-admin

--- src/index.js
+++ src/index.js
@@ -1,76 +1,93 @@
 const jwt = require('jsonwebtoken');
 class Auth {
-  constructor(secretKey){
-      this.secretKey = secretKey;
+  constructor(secretKey) {
+    this.secretKey = secretKey;
   }
-  async verify(token){
-      return new Promise((resolve,reject)=>{
-         jwt.verify(token,this.secretKey,function (err,decoded){
-              if(err != null){
-                  return reject(err);
-              }
-              return resolve(createUser(decoded));
-          })
+  async verify(token) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, this.secretKey, function (err, decoded) {
+        if (err != null) {
+          return reject(err);
+        }
+        return resolve(createUser(decoded));
       })
-    
+    })
+
   }
-  async createToken({type,data}){
-      return new Promise((resolve,reject)=>{
-         jwt.sign({
-          type,
-          data
-        },this.secretKey,{
-           expiresIn:'1d',
-         },function (err,encoded){
-           if(err != null){
-             return reject(err);
-           }
-           resolve(encoded);
-         })
-      });
-   }
+  async createToken({ type, data }) {
+    return new Promise((resolve, reject) => {
+      jwt.sign({
+        type,
+        data
+      }, this.secretKey, {
+        expiresIn: '1d',
+      }, function (err, encoded) {
+        if (err != null) {
+          return reject(err);
+        }
+        resolve(encoded);
+      })
+    });
+  }
 }
 module.exports = Auth;
 
-function createUser(o){
-  if(o == null) throw new Error('user is not defined');
-    let {type,data} = o;
-    return {
-        info:data,
-        type,
-        isCustomer(){
-          return type === 'customer';
-        },
-        isPersonnel(){
-          return type === 'personnel';
-        }
+function createUser(o) {
+  if (o == null) throw new Error('user is not defined');
+  let { type, data } = o;
+  return {
+    info: data,
+    type,
+    isCustomer() {
+      return type === 'customer';
+    },
+    isPersonnel() {
+      return type === 'personnel';
     }
+  }
 }
 
-module.exports.jwt = function ({secret}){
-  return function (req,res,next){
-    if(req.method === 'OPTIONS'){
+module.exports.jwt = function ({ secret }) {
+  return function (req, res, next) {
+    if (req.method === 'OPTIONS') {
       next();
       return;
     }
     const authHeader = req.headers.authorization;
     if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        if(token == null){
-          res.sendStatus(401);
-          return;
-        }
-        new Auth(secret).verify(token)
-        .then((user)=>{
+      const token = authHeader.split(' ')[1];
+      if (token == null) {
+        res.sendStatus(401);
+        return;
+      }
+      new Auth(secret).verify(token)
+        .then((user) => {
           req.user = user;
           next();
-        }).catch(()=>{
-           res.sendStatus(401);
+        }).catch(() => {
+          res.sendStatus(401);
         })
     } else {
-        res.sendStatus(401);
+      res.sendStatus(401);
     }
   }
+}
+
+function getUser(req, secret) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return Promise.reject();
+  }
+  const token = authHeader.split(' ')[1];
+  if (token == null) {
+    res.sendStatus(401);
+    return;
+  }
+  return new Auth(secret)
+    .verify(token)
+    .then((user) => {
+      return user;
+    });
 }
 
 /**
@@ -79,15 +96,15 @@ module.exports.jwt = function ({secret}){
  * @param {Response} res 
  * @param {*} next 
  */
-module.exports.isAdmin = function (req,res,next){
-  if(req.user == null){
-    res.status(401).end();
-    return;
-  }
-   let isAdmin =  typeof req.user.isPersonnel == "function" && req.user.isPersonnel();
-   if(!isAdmin){
-      res.status(403).end();
-      return;
-   }
-   next();
+module.exports.isAdmin = function (req, res, next) {
+  getUser(req,req.secretKey).then((user)=>{
+      let isAdmin = typeof user.isPersonnel == "function" && user.isPersonnel();
+      if (!isAdmin) {
+        res.status(403).end();
+        return;
+      }
+      next();
+  }).catch(()=>{
+      res.status(401).end();
+  })
 }
